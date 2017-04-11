41e998715eb08d547b3d2a17bbf86e830d29a1ad
privatejw <privatejw@gmail.com>
2017-04-12 01:40:06 +0800

    added title and credits scene, with music

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+
+	public void StartLocalGame() {
+		Debug.Log("start local game");
+		NetworkManager.singleton.StartHost();
+	}
+	
+	public void JoinLocalGame() {
+		if (hostNameInput.text != "Hostname") {
+			NetworkManager.singleton.networkAddress = hostNameInput.text;
+		}	
+		NetworkManager.singleton.StartClient();
+	}
+	
+	void StartMatchMaker() {
+		NetworkManager.singleton.StartMatchMaker();
+	}
+	
+	public UnityEngine.UI.Text hostNameInput;
+
+
+	void Start() {
+		hostNameInput.text = NetworkManager.singleton.networkAddress;
+	}
+
+	public void Credits() {
+		//Debug.Log("Display credits");
+		SceneManager.LoadScene("CreditsScene");
+	}
+}
