4715e8512470493cd81961e5e8a4589ae9b0848f
CrimRabbit <tyclian@hotmail.com>
2017-04-13 01:43:29 +0800

    Update Arrow.cs

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour {
-	public int direction; //0 is left, 1 is right
+	public int direction; //-1 is left, 1 is right
 	public Sprite normal;
 	public Sprite highlighted;
 
