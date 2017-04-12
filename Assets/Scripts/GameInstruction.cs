62d39469e6623c05e3431f652bebdba7a24bc804
CrimRabbit <tyclian@hotmail.com>
2017-04-12 21:24:48 +0800

    added more names, & wirecutters/wires

--- Assets/Scripts/GameInstruction.cs
+++ Assets/Scripts/GameInstruction.cs
@@ -9,16 +9,22 @@ public class GameInstruction {
 	string displayText;
 
 	public string[,] toolNames = {
+		{"Phillips Screwdriver", "Plus Turnscrew", "Cross Rivet"},
+		{"Flat Head Screwdriver", "Pokeball Turnscrew", "Straight Rivet"},
 		{"3-Way Screwdriver", "Mecedes Turnscrew", "Tri-point Rivet"},
 		{"5-Way Screwdriver", "Star Turnscrew", "Five-point Rivet"},
-		{"Flat Head Screwdriver", "Pokeball Turnscrew", "Straight Rivet"},
-		{"Phillips Screwdriver", "Plus Turnscrew", "Cross Rivet"},
 		{"Square Screwdriver", "Cubic Turnscrew", "Four-sided Rivet"},
 		{"Hexagonal Wrench", "Benzene Kernel", "Honeycomb Cranium"},
 		{"Pentagonal Wrench", "Penta Kernel", "Pentangular Cranium"},
 		{"Square Wrench", "Cubic Kernel", "Four-sided Cranium"},
-		{"Star Wrench", "Paragon Kernel", "Twinkle Little Cranium"},
 		{"Triangle Wrench", "Trilateral Kernel", "Trigon Cranium"},
+		{"Star Wrench", "Paragon Kernel", "Twinkle Little Cranium"},
+		{"Red Wirecutter", "Blood Scissors", "Lipoplasmic Scalper"},
+		{"Green Wirecutter", "Goop Severer", "Tree Blood Knife"},
+		{"Navy Wirecutter", "Silence of the Ocean", "Endowharfic Stopper"},
+		{"Cyan Wirecutter", "Sky Wirecutter", "Endicular Blocker"},
+		{"Purple Wirecutter", "Magenta Wirecutter", "Smackadorfal Repeader"},
+		{"Yellow Wirecutter", "Honey Wirecutter", "Lemon Tea Tightener"},
 	};
 
 	public string[,] action = {
@@ -32,19 +38,31 @@ public class GameInstruction {
 		{"Loosen the ", "Mash the ", "Tighten the ", "Screw the "},
 		{"Loosen the ", "Mash the ", "Tighten the ", "Screw the "},
 		{"Loosen the ", "Mash the ", "Tighten the ", "Screw the "},
+		{"Cut the", "Sever the", "Disassemble the", "Cleave the"},
+		{"Cut the", "Sever the", "Disassemble the", "Cleave the"},
+		{"Cut the", "Sever the", "Disassemble the", "Cleave the"},
+		{"Cut the", "Sever the", "Disassemble the", "Cleave the"},
+		{"Cut the", "Sever the", "Disassemble the", "Cleave the"},
+		{"Cut the", "Sever the", "Disassemble the", "Cleave the"},
 	};
 
 	public string[,] targetNames = {
+		{"Phillips Bolt", "Plus Pin", "Cross Rivet"},
+		{"Flat Head Bolt", "Pokeball Pin", "Straight Rivet"},
 		{"3-Way Bolt", "Mecedes Pin", "Tri-point Rivet"},
 		{"5-Way Bolt", "Star Pin", "Five-point Rivet"},
-		{"Flat Head Bolt", "Pokeball Pin", "Straight Rivet"},
-		{"Phillips Bolt", "Plus Pin", "Cross Rivet"},
 		{"Square Bolt", "Cubic Pin", "Four-sided Rivet"},
 		{"Hexagonal Nut", "Benzene Kernel", "Honeycomb Cranium"},
 		{"Pentagonal Nut", "Penta Kernel", "Pentangular Cranium"},
 		{"Square Nut", "Cubic Kernel", "Four-sided Cranium"},
-		{"Star Nut", "Paragon Kernel", "Twinkle Little Cranium"},
 		{"Triangle Nut", "Trilateral Kernel", "Trigon Cranium"},
+		{"Star Nut", "Paragon Kernel", "Twinkle Little Cranium"},
+		{"Red Wire", "Red String of Fate", "Lipoplasmic Transferrer"},
+		{"Green Wire", "Goop Transferrer", "Tree Blood Pipe"},
+		{"Navy Wire", "Song of the Ocean", "Endowharfic Amplifier"},
+		{"Cyan Wire", "Sky Wire", "Endicular Rectifier"},
+		{"Purple Wire", "Magenta Wire", "Smackadorfal Lamber"},
+		{"Yellow Wire", "Honey Wire", "Lemon Tea Squishy Pipe"},
 	};
 
 	public GameInstruction(int tool, int target, int targetValue) {
